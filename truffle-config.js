8475e3c2ffdfe24c2466b46d066c73b2ba394bb4
james-c <j.a.cunningham@gmail.com>
2021-06-25 12:23:21 +0100

    Add correct network id for rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -61,7 +61,7 @@ module.exports = {
       // NB: It's important to wrap the provider as a function.
       rinkeby: {
           provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/` + key, 0),
-          network_id: 3,       // Ropsten's id
+          network_id: 4,       // Ropsten's id
           gas: 5500000,        // Ropsten has a lower block limit than mainnet
           confirmations: 2,    // # of confs to wait between deployments. (default: 0)
           timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
