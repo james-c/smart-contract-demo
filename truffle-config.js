ff340591ef19f1a9b28d9d76b166ce2081ff7ede
james-c <j.a.cunningham@gmail.com>
2021-06-25 12:21:10 +0100

    Update network name in config

--- truffle-config.js
+++ truffle-config.js
@@ -59,8 +59,8 @@ module.exports = {
       // },
       // Useful for deploying to a public network.
       // NB: It's important to wrap the provider as a function.
-      ropsten: {
-          provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/` + key),
+      rinkeby: {
+          provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/` + key, 0),
           network_id: 3,       // Ropsten's id
           gas: 5500000,        // Ropsten has a lower block limit than mainnet
           confirmations: 2,    // # of confs to wait between deployments. (default: 0)
